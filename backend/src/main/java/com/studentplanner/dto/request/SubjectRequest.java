1035eb03851b42c292f010c81bab5fd9d82b45bc
MihisaraNet <isuimk@gmail.com>
2026-05-18 09:23:46 +0530

    chore: add/update SubjectRequest.java

--- backend/src/main/java/com/studentplanner/dto/request/SubjectRequest.java
+++ backend/src/main/java/com/studentplanner/dto/request/SubjectRequest.java
@@ -1,2 +1,22 @@
-﻿package com.studentplanner.dto.request;
-// SubjectRequest DTO
+package com.studentplanner.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class SubjectRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Code is required")
+    private String code;
+
+    @NotNull(message = "Credits are required")
+    private Integer credits;
+
+    @NotBlank(message = "Semester is required")
+    private String semester;
+    
+    private String grade;
+}
